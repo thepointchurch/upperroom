a8f347e020d8b17739e8c4a4ffa1cfa2edb09f0d
Craig Holyoak <craig@helmsdeep.org>
2015-05-30 11:59:38 +1000

    Added necessary imports for viewing newsletter issues.

--- apps/newsletter/views.py
+++ apps/newsletter/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views import generic
 
