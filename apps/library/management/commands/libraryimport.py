c968b75d8b8b246670c387f8c4109be22728e6e2
Craig Holyoak <craig@helmsdeep.org>
2015-06-06 11:30:03 +1000

    Tidy up library data on import.

--- apps/library/management/commands/libraryimport.py
+++ apps/library/management/commands/libraryimport.py
@@ -35,11 +35,11 @@ class Command(BaseCommand):
                 self.stderr.write('WARNING: Book has no title: ' % line)
                 continue
 
-            book = Book(title=line[0],
-                        subtitle=line[1],
-                        description=line[2],
-                        type=line[3],
-                        author=line[4],
-                        isbn=line[5],
-                        location=line[6])
+            book = Book(title=line[0].strip().replace('', '\n'),
+                        subtitle=line[1].strip().replace('', '\n'),
+                        description=line[2].strip().replace('', '\n'),
+                        type=line[3].strip().replace('', '\n'),
+                        author=line[4].strip().replace('', '\n'),
+                        isbn=line[5].strip().replace('', '\n'),
+                        location=line[6].strip().replace('', '\n'))
             book.save()
