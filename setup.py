9890707194ccbbe42f5e33922e5f6fc29053fe82
Craig Holyoak <craig@helmsdeep.org>
2020-01-08 09:23:38 +1000

    Ugraded to Django 3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='thepoint',
-    version='2.0.1',
+    version='2.2.0',
     license=open('LICENSE').read(),
     description='''A Django project for The Point Church's website.''',
     long_description=README,
@@ -18,7 +18,7 @@ setup(
     author='Craig Holyoak',
     author_email='craig@helmsdeep.org',
     install_requires=[
-        'Django>=2.1',
+        'Django>=3.0',
         'django-storages',
     ],
     classifiers=[
@@ -31,8 +31,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
