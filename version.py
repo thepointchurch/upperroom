ecac938af37d28a4d994e6b1a67ab77f76e0ae7b
Craig Holyoak <craig@helmsdeep.org>
2020-08-13 07:18:33 +1000

    Further refinements of the build system

--- version.py
+++ version.py
@@ -5,11 +5,11 @@ def get_version():
     try:
         branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD')).strip().decode()
         long_version = subprocess.check_output(('git', 'describe', '--tags', '--long')).strip().decode()
-        if branch == 'master':
-            version, rev, commit = long_version.rsplit('-', 2)
-            if rev == '0':
-                return version
-            elif version.count('.') == 1:
+        version, rev, commit = long_version.rsplit('-', 2)
+        if rev == '0':
+            return version
+        if branch.startswith('master'):
+            if version.count('.') == 1:
                 return version + '.0-r' + rev
             else:
                 return version + '-r' + rev
@@ -20,6 +20,11 @@ def get_version():
         sys.path.append('.')
         import thepoint
         return thepoint.__version__
+
+
+def update_version():
+    version = get_version()
+    subprocess.check_call(('sed', '-i', "s/^__version__ = .*$/__version__ = '%s'/" % version, 'thepoint/__init__.py'))
 
 
 if __name__ == '__main__':
