01ee0a4c6a0b632fe3157dd256bc2497aa20c92d
Craig Holyoak <craig@helmsdeep.org>
2021-01-03 12:13:14 +1000

    Improved noise reduction for podcast audio

--- bin/thepoint-podcast-audio.py
+++ bin/thepoint-podcast-audio.py
@@ -3,13 +3,16 @@
 
 import argparse
 import json
+import os
 import subprocess
 import sys
+import tempfile
 import time
 
 parser = argparse.ArgumentParser(description="Normalise a podcast file.")
-parser.add_argument("--low-pass", dest="low_pass", type=int, default=3000, help="low pass level")
-parser.add_argument("--high-pass", dest="high_pass", type=int, default=200, help="high pass level")
+parser.add_argument("--noise-start", dest="noise_start", default="00:00:00", help="noise sample start")
+parser.add_argument("--noise-length", dest="noise_length", default="00:00:01", help="noise sample length")
+parser.add_argument("--noise-level", dest="noise_level", type=float, default=0.21, help="noise level")
 parser.add_argument("--i", dest="target_i", type=float, default=-16.0, help="target I")  # -24.0
 parser.add_argument("--tp", dest="target_tp", type=float, default=-1.5, help="target TP")  # -2.0
 parser.add_argument("--lra", dest="target_lra", type=float, default=11.0, help="target LRA")
@@ -25,30 +28,75 @@ parser.add_argument("output_file")
 args = parser.parse_args()
 
 
-sys.stdout.write("Analysing loudness ...")
+sys.stdout.write("Analysing noise ...")
 sys.stdout.flush()
-p = subprocess.run(
+_, noise_profile = tempfile.mkstemp()
+ffmpeg = subprocess.Popen(
     (
         "ffmpeg",
         "-hide_banner",
+        "-loglevel",
+        "error",
+        "-nostdin",
+        "-y",
+        "-i",
+        args.input_file,
+        "-ss",
+        args.noise_start,
+        "-t",
+        args.noise_length,
+        "-f",
+        "aiff",
+        "-",
+    ),
+    stdout=subprocess.PIPE,
+)
+sox = subprocess.Popen(("sox", "-q", "-t", "aiff", "-", "-n", "noiseprof", noise_profile,), stdin=ffmpeg.stdout,)
+sox.communicate()
+print(" done")
+
+
+sox_cmd = (
+    "sox",
+    "-q",
+    args.input_file,
+    "-t",
+    "aiff",
+    "-",
+    "noisered",
+    noise_profile,
+    str(args.noise_level),
+)
+sys.stdout.write("Analysing loudness ...")
+sys.stdout.flush()
+sox = subprocess.Popen(sox_cmd, stdout=subprocess.PIPE)
+ffmpeg = subprocess.Popen(
+    (
+        "ffmpeg",
+        "-hide_banner",
+        "-nostdin",
         "-y",
         "-i",
         args.input_file,
         "-af",
-        ("loudnorm=I={target_i}:" "TP={target_tp}:" "LRA={target_lra}:" "print_format=json").format(
-            target_i=args.target_i, target_lra=args.target_lra, target_tp=args.target_tp
-        ),
+        (
+            "loudnorm=linear=true:"
+            "dual_mono=true:"
+            "I={target_i}:"
+            "TP={target_tp}:"
+            "LRA={target_lra}:"
+            "print_format=json"
+        ).format(target_i=args.target_i, target_lra=args.target_lra, target_tp=args.target_tp),
         "-f",
         "null",
         "-",
     ),
+    stdin=sox.stdout,
     stderr=subprocess.PIPE,
-    check=True,
 )
+_, loudness_stats = ffmpeg.communicate()
 print(" done")
-stats = json.loads(b"".join(p.stderr.splitlines()[-12:]).decode())
-stats["lowpass"] = args.low_pass
-stats["highpass"] = args.high_pass
+stats = json.loads(b"".join(loudness_stats.splitlines()[-12:]).decode())
 
 metadata = [
     "-metadata",
@@ -63,17 +111,21 @@ if args.title:
 if args.speaker:
     metadata += ["-metadata", "artist=%s" % args.speaker]
 
-subprocess.run(
+codec = {"mp3": "libmp3lame", "m4a": "libfdk_aac"}[args.output_file.split(".")[-1]]
+
+sox = subprocess.Popen(sox_cmd, stdout=subprocess.PIPE)
+ffmpeg = subprocess.Popen(
     [
         "ffmpeg",
         "-hide_banner",
+        "-nostdin",
         "-y",
         "-i",
-        args.input_file,
+        "-",
         "-ac",
         "1",
         "-c:a",
-        "libmp3lame",
+        codec,
         "-b:a",
         args.bit_rate,
         "-ar",
@@ -81,15 +133,21 @@ subprocess.run(
         "-af",
         (
             "loudnorm=linear=true:"
+            "dual_mono=true:"
+            "I={target_i}:"
+            "TP={target_tp}:"
+            "LRA={target_lra}:"
+            "offset={target_offset}:"
             "measured_I={input_i}:"
             "measured_LRA={input_lra}:"
             "measured_tp={input_tp}:"
-            "measured_thresh={input_thresh},"
-            "lowpass={lowpass},"
-            "highpass={highpass}"
-        ).format(**stats),
+            "measured_thresh={input_thresh}"
+        ).format(target_i=args.target_i, target_lra=args.target_lra, target_tp=args.target_tp, **stats),
     ]
     + metadata
     + [args.output_file],
-    check=False,
+    stdin=sox.stdout,
 )
+ffmpeg.communicate()
+
+os.unlink(noise_profile)
