2c3be2e94231e9dd51400c30c5e475fba52c5229
Craig Holyoak <craig@helmsdeep.org>
2021-02-06 12:27:07 +1000

    Fixed migrations replacing the attachment primary keys

--- thepoint/weblog/migrations/0002_attachment_uuid.py
+++ thepoint/weblog/migrations/0002_attachment_uuid.py
@@ -3,6 +3,7 @@
 import uuid
 
 from django.db import migrations, models
+from django.db.utils import OperationalError
 
 
 def set_uuid(apps, schema_editor):
@@ -11,6 +12,28 @@ def set_uuid(apps, schema_editor):
     for obj in Attachment.objects.using(db_alias).all():
         obj.uuid = uuid.uuid4()
         obj.save()
+
+
+def remove_old_primary_key(apps, schema_editor):
+    Attachment = apps.get_model("weblog", "Attachment")
+    connection = schema_editor.connection
+    with connection.cursor() as cursor:
+        constraints = connection.introspection.get_constraints(cursor, Attachment._meta.db_table)
+    key_constraint = None
+    for constraint_name, constraint in constraints.items():
+        if constraint.get("primary_key"):
+            key_constraint = constraint_name
+            break
+    else:
+        raise Exception("Missing primary key constraint")
+    quote_name = connection.ops.quote_name
+    try:
+        schema_editor.execute(
+            schema_editor.sql_delete_pk
+            % {"table": quote_name(Attachment._meta.db_table), "name": quote_name(key_constraint)}
+        )
+    except OperationalError:
+        pass  # assume the DB was SQLite
 
 
 class Migration(migrations.Migration):
@@ -22,6 +45,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(model_name="attachment", name="uuid", field=models.UUIDField(null=True),),
         migrations.RunPython(set_uuid, migrations.RunPython.noop),
+        migrations.RunPython(remove_old_primary_key),
         migrations.AlterField(
             model_name="attachment",
             name="uuid",
