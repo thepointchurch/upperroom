47d1faf11c429134236d7dc768fd513a9606f5bb
Craig Holyoak <craig@helmsdeep.org>
2021-04-22 16:18:54 +1000

    Fixed pylint check

--- upperroom/members/middleware.py
+++ upperroom/members/middleware.py
@@ -1,4 +1,4 @@
-class UsernameHeaderMiddleware:
+class UsernameHeaderMiddleware:  # pylint: disable=too-few-public-methods
     def __init__(self, get_response):
         self.get_response = get_response
 
