3b2b5f2cee6a38af80f4bd22dfaaf5420347e630
Craig Holyoak <craig@helmsdeep.org>
2021-05-07 16:49:03 +1000

    Don't overwrite existing title fields when selecting an attachment file

--- upperroom/resources/static/scripts/resources/admin_attachment_title.js
+++ upperroom/resources/static/scripts/resources/admin_attachment_title.js
@@ -2,8 +2,10 @@
     $(function() {
         $('input[id^="id_attachments-"][id$="-file"]').change(function() {
             var title_field = $("#" + this.id.replace(/-file$/, "-title"));
-            title_field.val(this.value.split(/[\\\/]/).pop().replace(/\.[^/.]+$/, ""));
-            title_field.trigger("change");
+            if (!title_field.val()) {
+                title_field.val(this.value.split(/[\\\/]/).pop().replace(/\.[^/.]+$/, ""));
+                title_field.trigger("change");
+            }
         });
     });
 })(django.jQuery);
