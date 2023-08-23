002bafe16f61c5d53831acb8bd8aa7c5632b9b77
Craig Holyoak <craig@helmsdeep.org>
2023-08-23 17:29:45 +1000

    Fixed logic for multimedia anchor text

--- upperroom/utils/media.py
+++ upperroom/utils/media.py
@@ -11,5 +11,7 @@ def get_mime_anchor(mime_type):
 
 
 def is_video_embed(body):
-    video_embed = re.compile(r"<iframe [^>]+youtube[^>]+/embed/>")
-    return bool([x for x in body.splitlines() if x and not video_embed.search(x)])
+    video_embed = re.compile(r"<iframe [^>]+youtube[^>]+/embed/")
+    lines = [x for x in body.splitlines() if x]
+    non_embed_lines = [x for x in lines if not video_embed.search(x)]
+    return len(lines) != len(non_embed_lines)
